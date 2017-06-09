6c80ea930afa7f4c6cd3b0cf9fbb15a346969412
Jacob Barnes <jmbarnes1987@gmail.com>
2017-06-09 14:48:30 -0500

    Added ImageHelper Class Testing Various Code

--- LoveReminder/LoveReminder/MainActivity.cs
+++ LoveReminder/LoveReminder/MainActivity.cs
@@ -1,6 +1,12 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
+using System.Collections.ObjectModel;
+using Android.Provider;
+using Android.Net;
+using static Android.Resource;
+using LoveReminder.Utility;
 
 namespace LoveReminder
 {
@@ -13,9 +19,20 @@ namespace LoveReminder
 
             SetContentView (Resource.Layout.Main);
             TextView textView = FindViewById < TextView>(Resource.Id.LoveQuoteTextView);
+            ImageView imageView = FindViewById<ImageView>(Resource.Id.LoveReminderImageView);
             Button button = FindViewById<Button>(Resource.Id.ReminderButton);
-            button.Click +=
-                delegate { textView.Text = "It Works!"; };
+
+            button.Click += delegate {
+                textView.Text = "It Works!";
+                //var imageIntent = new Intent();
+                //imageIntent.SetType("image/*");
+                //imageIntent.SetAction(Intent.);
+                //Uri image = MediaStore.Images.Media.ExternalContentUri;
+                var imageBitmap = ImageHelper.GetImageBitmapFromUrl("https://scontent-dft4-2.xx.fbcdn.net/v/t1.0-9/16939139_10154989976651192_2807782103431374592_n.jpg?oh=049b4bc179e4a774b62642dcd5df42c4&oe=59E76E32");
+
+                //
+                imageView.SetImageBitmap(imageBitmap);
+            }; 
         }
     }
 }
