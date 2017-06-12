170de68766c47a8e8384f60386f6cd4e3ce7a830
Jacob Barnes <jmbarnes1987@gmail.com>
2017-06-11 23:02:41 -0500

    Aligned layout of elements

--- LoveReminder/LoveReminder/Resources/Resource.Designer.cs
+++ LoveReminder/LoveReminder/Resources/Resource.Designer.cs
@@ -60,14 +60,17 @@ namespace LoveReminder
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int LoveQuoteTextView = 2131034114;
+			
 			// aapt resource value: 0x7f050001
-			public const int LoveQuoteTextView = 2131034113;
+			public const int LoveReminderImageView = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int ReminderButton = 2131034115;
 			
 			// aapt resource value: 0x7f050000
-			public const int LoveReminderImageView = 2131034112;
-			
-			// aapt resource value: 0x7f050002
-			public const int ReminderButton = 2131034114;
+			public const int mainLinearLayout = 2131034112;
 			
 			static Id()
 			{
